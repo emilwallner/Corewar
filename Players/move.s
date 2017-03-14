baf7641e0eff382cc6110aea87a0232eb3678924
Emil Wallner <w@llner.co>
2017-03-14 16:42:52 +0100

    players

--- Players/move.s
+++ Players/move.s
@@ -1,2 +1,4 @@
 .name "Yo dang!"
 .comment "Zup, why you lookin at me like that?"
+
+zjmp	%1, r3
---
+++ Players/zork.s
@@ -0,0 +1,20 @@
+.name "zork"
+.comment "I'M ALIIIIVE"
+
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+#		ld %255, r2
+#		st r1, 256
+#		st r1, 511
+#		st r1, 513
+# l2:
+#		sti r1, %:live, %1
+#		and r1, %1, r1
+#
+# live:
+#		live %1
+#		zjmp %:live
