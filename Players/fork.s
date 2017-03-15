5779b20e9c7e063d3351a1660544e72efb437f59
HAZIZA <mhaziza@student.42.fr>
2017-03-15 14:25:53 +0100

    2 players

---
+++ Players/fork.s
@@ -0,0 +1,11 @@
+.name "zork"
+.comment "just a basic living prog"
+
+l2:	add r2,r3,r4
+	fork %10
+
+live:	live	%-3
+	zjmp %:live
+
+live2: live %-3
+	zjmp %:live2
---
+++ Players/id_player_dyn.s
@@ -0,0 +1,8 @@
+.name "zork"
+.comment "just a basic living prog"
+
+st r1, 11
+l2:	add r2,r3,r4
+
+live:	live	%10
+	zjmp %:live
