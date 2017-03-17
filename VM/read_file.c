688b23906d8175c967d6e43106b23e20e78ebde8
Emil Wallner <w@llner.co>
2017-03-17 15:00:18 +0100

    Clone asm

--- VM/read_file.c
+++ VM/read_file.c
@@ -1,104 +1,107 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   read_file.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ewallner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/03/17 14:16:53 by ewallner          #+#    #+#             */
-/*   Updated: 2017/03/17 14:16:58 by ewallner         ###   ########.fr       */
+/*   Created: 2017/03/17 14:16:11 by ewallner          #+#    #+#             */
+/*   Updated: 2017/03/17 14:16:16 by ewallner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "vm.h"
 
-int read_file() 
+static int	free_and_return(char *file, char **asm_tab, int free_tab, int ret)
+{
+	int	i;
 
+	free(file);
+	i = -1;
+	while (free_tab && asm_tab[++i])
+		free(asm_tab[i]);
+	if (free_tab)
+		free(asm_tab);
+	if (!ret)
+		ft_putstr("Error\n");
+	return (ret);
+}
 
+static void	counts_char_line(char *buff, t_read *tread)
+{
+	int	i;
 
+	i = -1;
+	while (buff[++i])
+		if (buff[i] == '\n')
+			tread->nb_line += 1;
+	tread->nb_char += i;
+}
 
+int			get_counts(char *file_name, t_read *tread)
+{
+	char	buff[BUFF_SIZE + 1];
+	int		ret;
+	int		fd;
 
+	tread->nb_char = 0;
+	tread->nb_line = 0;
+	buff[0] = 0;
+	if ((fd = open(file_name, O_RDONLY)) == -1)
+		return (0);
+	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[ret] = '\0';
+		counts_char_line(buff, tread);
+	}
+	if (close(fd) == -1)
+		return (0);
+	if (ret == -1)
+		return (0);
+	if (!tread->nb_char || tread->nb_line < 4)
+		return (0);
+	return (1);
+}
 
+int			get_asm(char *file_name, char *file, t_read *tread)
+{
+	int	fd;
 
+	if ((fd = open(file_name, O_RDONLY)) == -1)
+		return (0);
+	if (read(fd, file, tread->nb_char) == -1)
+		return (0);
+	file[tread->nb_char] = '\0';
+	if (close(fd) == -1)
+		return (0);
+	return (1);
+}
 
+int			split_asm(char **asm_tab, char *file, int nb)
+{
+	int	i;
+	int	line;
+	int	cursor;
 
-
-// static void	counts_char_line(char *buff, t_read *tread)
-// {
-// 	int	i;
-//
-// 	i = -1;
-// 	while (buff[++i])
-// 		if (buff[i] == '\n')
-// 			tread->nb_line += 1;
-// 	tread->nb_char += i;
-// }
-//
-// int			get_counts(char *file_name, t_read *tread)
-// {
-// 	char	buff[BUFF_SIZE + 1];
-// 	int		ret;
-// 	int		fd;
-//
-// 	tread->nb_char = 0;
-// 	tread->nb_line = 0;
-// 	buff[0] = 0;
-// 	if ((fd = open(file_name, O_RDONLY)) == -1)
-// 		return (0);
-// 	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
-// 	{
-// 		buff[ret] = '\0';
-// 		counts_char_line(buff, tread);
-// 	}
-// 	if (close(fd) == -1)
-// 		return (0);
-// 	if (ret == -1)
-// 		return (0);
-// 	if (!tread->nb_char || tread->nb_line < 4)
-// 		return (0);
-// 	return (1);
-// }
-//
-// int			get_asm(char *file_name, char *file, t_read *tread)
-// {
-// 	int	fd;
-//
-// 	if ((fd = open(file_name, O_RDONLY)) == -1)
-// 		return (0);
-// 	if (read(fd, file, tread->nb_char) == -1)
-// 		return (0);
-// 	file[tread->nb_char] = '\0';
-// 	if (close(fd) == -1)
-// 		return (0);
-// 	return (1);
-// }
-//
-// int			split_asm(char **asm_tab, char *file, int nb)
-// {
-// 	int	i;
-// 	int	line;
-// 	int	cursor;
-//
-// 	i = -1;
-// 	line = -1;
-// 	asm_tab[0] = NULL;
-// 	while (++line < nb && ++i >= 0)
-// 	{
-// 		cursor = 0;
-// 		while (file[i + cursor] && file[i + cursor] != '\n')
-// 			cursor++;
-// 		if (!(asm_tab[line] = ft_strnew(cursor + 1)))
-// 			return (0);
-// 		cursor = 0;
-// 		while (file[i] && file[i] != '\n')
-// 		{
-// 			asm_tab[line][cursor] = file[i];
-// 			cursor++;
-// 			i++;
-// 		}
-// 		asm_tab[line][cursor] = '\0';
-// 	}
-// 	asm_tab[line] = NULL;
-// 	return (1);
-// }
+	i = -1;
+	line = -1;
+	asm_tab[0] = NULL;
+	while (++line < nb && ++i >= 0)
+	{
+		cursor = 0;
+		while (file[i + cursor] && file[i + cursor] != '\n')
+			cursor++;
+		if (!(asm_tab[line] = ft_strnew(cursor + 1)))
+			return (0);
+		cursor = 0;
+		while (file[i] && file[i] != '\n')
+		{
+			asm_tab[line][cursor] = file[i];
+			cursor++;
+			i++;
+		}
+		asm_tab[line][cursor] = '\0';
+	}
+	asm_tab[line] = NULL;
+	return (1);
+}
--- VM/vm.h
+++ VM/vm.h
@@ -24,25 +24,10 @@ typedef struct	s_read
 	int	nb_line;
 }				t_read;
 
-typedef struct	s_label
-{
-	char	*label;
-	int		index;
-}				t_label;
-
-typedef struct	s_asm
-{
-	char	**asm_tab;
-	t_label	*labels;
-	int		nb_labels;
-}				t_asm;
-
-typedef struct	s_instruction
-{
-	char	*name;
-	char	*params;
-	char	*label;
-}				t_instruction;
+static int	free_and_return(char *file, char **asm_tab, int free_tab, int ret);
+static void	counts_char_line(char *buff, t_read *tread);
+int			get_asm(char *file_name, char *file, t_read *tread);
+int			split_asm(char **asm_tab, char *file, int nb);
 
 
 #endif
