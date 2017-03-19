5c29e6d4a7d134951a0bac7ade47c23a0ae682f5
Natansab <natansab@gmail.com>
2017-03-19 19:23:53 +0100

    check args

--- VM/ft_check_args.c
+++ VM/ft_check_args.c
@@ -6,11 +6,20 @@
 /*   By: nsabbah <nsabbah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/19 18:26:25 by nsabbah           #+#    #+#             */
-/*   Updated: 2017/03/19 19:08:18 by nsabbah          ###   ########.fr       */
+/*   Updated: 2017/03/19 19:23:35 by nsabbah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
+
+/*
+** If the last to digit are not blank => error
+** Get the number of args and check if it's the one we were waiting for.
+** Check if each args are valid
+**
+** We'll have to figure out how we handle the errors and what to do with the
+** data that we get
+*/
 
 void	ft_check_args(t_env e, int opcode, int acb)
 {
@@ -19,19 +28,17 @@ void	ft_check_args(t_env e, int opcode, int acb)
 
 	i = -1;
 	nb_of_args = 0;
+
 printf("for opcode is %i and acb is %#x\n", opcode, acb);
-/*
-** Get the number of args and check if it's the one we were waiting for.
-*/
+
+if (acb & 0x3)
+	printf("ERROR\n");
 	while (++i < 3)
 		if (acb >> (6 - i * 2) & 0x3)
 			nb_of_args++;
 	if (nb_of_args != e.op_tab[opcode].nb_params)
 		printf("ERROR NB OF ARGS");
 	printf("nb_of_args is %i\n", nb_of_args);
-/*
-** Check if each args are valid
-*/
 	i = 0;
 	while (i < nb_of_args)
 	{
