1b1d1854f8c9a5cd2594c43cdc4ea0198199aae0
Timothy LENGLINE <tlenglin@e1r4p6.42.fr>
2017-03-20 18:37:17 +0100

    prepull

--- Players/test.s
+++ Players/test.s
@@ -1,21 +1,18 @@
 .name "viko"
 .comment "i_am_alive"
 
-#coucou
 st r1,12456
 ld %0,r2
 st r1,211
-st r1,211 #cou+cou
-st r1,211
-st r1,211
-st r1,211
-st r1,211
-st r1,211     #coucou
-st r1,211
-st r1,211
-st r1,211
-st r1,211
 #coucou
 st r1,211
-
-#coucou
+st r1,211
+st r1,211
+st r1,211
+st r1,211
+st r1,211
+st r1,211
+st r1,211
+st r1,211
+st r1,211
+st r1,211 #coucou
