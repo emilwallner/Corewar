7fad94b6f26cb08561c5fa514fe1573f45a9a162
Timothy LENGLINE <tlenglin@e1r3p6.42.fr>
2017-03-21 16:44:31 +0100

    script shell

--- Players/test_xor_instruction.s
+++ Players/test_xor_instruction.s
@@ -3,54 +3,18 @@
 
 xor r2,10,r3
 xor r2,:label,r3
+xor r2,r5,r3
 xor %6,10,r3
 xor %6,:label2,r3
+xor %6,r5,r3
 xor %:label,10,r3
 xor %:label,:label2,r3
+xor %:label,r5,r3
 xor :label,10,r3
 xor :label,:label2,r3
-xor 10,10,r3
-xor 10,:label2,r3
-xor r2,r10,r3
-xor %6,r10,r3
+xor :label,r5,r3
 label:
-xor %:label,r10,r3
-xor :label,r10,r3
+xor 10,10,r3
 label2:
-xor 10,r10,r3
-
-xor r2,10,%2
-xor r2,:label3,%2
-xor %6,10,%2
-xor %6,:label2,%2
-xor %:label4,10,%2
-xor %:label,:label2,%2
-xor :label3,10,%2
-xor :label4,:label2,%2
-xor 10,10,%2
-xor 10,:label2,%2
-xor r2,r10,%2
-xor %6,r10,%2
-label3:
-xor %:label2,r10,%2
-xor :label,r10,%2
-label4:
-xor 10,r10,%2
-
-xor r2,10,%:label2
-xor r2,:label3,%:label2
-xor %6,10,%:label2
-xor %6,:label2,%:label2
-xor %:label4,10,%:label2
-xor %:label,:label2,%:label2
-xor :label3,10,%:label2
-xor :label4,:label2,%:label2
-xor 10,10,%:label2
-xor 10,:label2,%:label2
-xor r2,r10,%:label2
-xor %6,r10,%:label2
-label3:
-xor %:label2,r10,%:label2
-xor :label,r10,%:label2
-label4:
-xor 10,r10,%:label2
+xor 10,:label2,r3
+xor 10,r5,r3
