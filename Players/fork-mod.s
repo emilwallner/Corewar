0b1c32098cad214d1abbf3f1c14b479c169e56c6
Natansab <natansab@gmail.com>
2017-03-23 10:44:04 +0100

    ld

---
+++ Players/fork-mod.s
@@ -0,0 +1,4 @@
+.name   "tim"
+.comment "just_a_basic_living_prog"
+
+fork %65500
---
+++ Players/lfork-mod.s
@@ -0,0 +1,8 @@
+.name   "tim"
+.comment "just_a_basic_living_prog"
+
+live %-1
+live %-1
+live %-1
+live %-1
+lfork %5
