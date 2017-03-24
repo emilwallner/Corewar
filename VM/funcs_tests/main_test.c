79ce5a5ed937dec4d631136654d850743fe01aa0
Natansab <natansab@gmail.com>
2017-03-24 17:37:26 +0100

    ft_add

--- VM/funcs_tests/main_test.c
+++ VM/funcs_tests/main_test.c
@@ -6,7 +6,7 @@
 /*   By: nsabbah <nsabbah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/21 17:23:13 by nsabbah           #+#    #+#             */
-/*   Updated: 2017/03/24 17:20:57 by nsabbah          ###   ########.fr       */
+/*   Updated: 2017/03/24 17:36:45 by nsabbah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,13 @@ void	ft_debug(t_env e)
 	ft_debug_lld(e, &cursor[0]);
 	ft_debug_lld2(e, &cursor[1]);
 
-	printf(RED "\n###### FUNCTION TEST : ft_lld.c ######\n\n" RESET);
+	printf(RED "\n###### FUNCTION TEST : ft_ldi.c ######\n\n" RESET);
 
 	ft_debug_ldi(e, &cursor[0]);
-	ft_debug_ldi2(e, &cursor[1]);}
+	ft_debug_ldi2(e, &cursor[1]);
+
+	printf(RED "\n###### FUNCTION TEST : ft_add.c ######\n\n" RESET);
+
+	ft_debug_add(e, &cursor[0]);
+	ft_debug_add2(e, &cursor[1]);
+}
