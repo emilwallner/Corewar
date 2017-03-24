c0af3fdd05b1a77389530daf1d09b9cb2140001c
Natansab <natansab@gmail.com>
2017-03-24 17:00:06 +0100

    MODA and MODX

--- VM/srcs/ft_cp_int.c
+++ VM/srcs/ft_cp_int.c
@@ -6,7 +6,7 @@
 /*   By: nsabbah <nsabbah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/24 16:07:53 by nsabbah           #+#    #+#             */
-/*   Updated: 2017/03/24 16:09:01 by nsabbah          ###   ########.fr       */
+/*   Updated: 2017/03/24 16:59:46 by nsabbah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ int		ft_cp_int(int int_start, t_env e)
 	i = 4;
 	value = 0;
 	while (--i >= 0)
-		value += e.a[int_start + (3 - i)].hex << (i * 8);
+		value += e.a[MODA(int_start + (3 - i))].hex << (i * 8);
 	return (value);
 }
