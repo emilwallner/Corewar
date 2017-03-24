ef520a4e2007ff14530fed443b0795323b8f1010
Emil Wallner <w@llner.co>
2017-03-24 16:51:30 +0100

    Cycle execute in end

--- VM/srcs/adjust_cycles.c
+++ VM/srcs/adjust_cycles.c
@@ -45,11 +45,15 @@ void ft_cycle_end_and_execute(t_env *e, t_cursor *cursor)
 
 	if(cursor->cycle == 0)
 	{
-		//ft_ld(e, cursor);
-		if(e->a[cursor->index].hex == 1)
-			(*func_ptr[1]) (e, cursor);
+		if(ft_check_args(*e, *cursor))
+		{
+			if(e->a[cursor->index].hex == 1)
+				(*func_ptr[1]) (e, cursor);
+			ft_update_cursor(e, cursor, cursor->comnd_len);
+		}
+		else
+			ft_update_cursor(e, cursor, 1);
 		cursor->running = 0;
-		ft_update_cursor(e, cursor, cursor->comnd_len);
 	}
 	else
 		cursor->cycle -= 1;
