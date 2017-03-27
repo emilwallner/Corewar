e131f439cf38bf2ce0e951847e4ad4289c0b9ad3
Natansab <natansab@gmail.com>
2017-03-27 20:14:24 +0200

    funcs and

--- VM/funcs/ft_and.c
+++ VM/funcs/ft_and.c
@@ -6,7 +6,7 @@
 /*   By: nsabbah <nsabbah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/20 17:14:24 by nsabbah           #+#    #+#             */
-/*   Updated: 2017/03/27 18:38:21 by nsabbah          ###   ########.fr       */
+/*   Updated: 2017/03/27 20:13:33 by nsabbah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,107 @@ void	ft_and(t_env *e, t_cursor *cursor)
 	int			r1;
 	int			r2;
 	int			r3;
+	int			ind;
 	char		acb;
 	int			value;
 
 	opc_ind = cursor->index;
 	acb = e->a[MODA(opc_ind + 1)].hex;
-	if ((acb & 0xFF) == RRR)
+	if (ZMASK(acb) == RRR)
 	{
 		r1 = e->a[MODA(opc_ind + 2)].hex;
 		r2 = e->a[MODA(opc_ind + 3)].hex;
 		r3 = e->a[MODA(opc_ind + 4)].hex;
 		if (ft_is_reg(r1, r2, r3))
+		{
 			cursor->reg[r3] = cursor->reg[r1] | cursor->reg[r2];
+			cursor->carry = 1;
+		}
 		cursor->index += 5;
+	}
+	else if (ZMASK(acb) == RDR)
+	{
+		r1 = e->a[MODA(opc_ind + 2)].hex;
+		r2 = e->a[MODA(opc_ind + 7)].hex;
+		if (ft_is_reg(r1, r2, r2))
+			cursor->reg[r2] = cursor->reg[r1] & get_dir(e, cursor, 3, 4);
+		cursor->index += 8;
+	}
+	else if (ZMASK(acb) == RIR)
+	{
+		r1 = e->a[MODA(opc_ind + 2)].hex;
+		r2 = e->a[MODA(opc_ind + 5)].hex;
+		if (ft_is_reg(r1, r2, r2))
+		{
+			cursor->reg[r2] = cursor->reg[r1] & get_ind(e, cursor, 3);
+			cursor->carry = 1;
+		}
+			cursor->index += 6;
+	}
+	else if (ZMASK(acb) == IRR)
+	{
+		r1 = e->a[MODA(opc_ind + 4)].hex;
+		r2 = e->a[MODA(opc_ind + 5)].hex;
+		if (ft_is_reg(r1, r2, r2))
+		{
+			cursor->reg[r2] = cursor->reg[r1] & get_ind(e, cursor, 2);
+			cursor->carry = 1;
+		}
+			cursor->index += 6;
+	}
+	else if (ZMASK(acb) == DRR)
+	{
+		r1 = e->a[MODA(opc_ind + 6)].hex;
+		r2 = e->a[MODA(opc_ind + 7)].hex;
+		if (ft_is_reg(r1, r2, r2))
+		{
+			cursor->reg[r2] = cursor->reg[r1] & get_dir(e, cursor, 2, 4);
+			cursor->carry = 1;
+		}
+			cursor->index += 8;
+	}
+	else if (ZMASK(acb) == DDR)
+	{
+		r1 = e->a[MODA(opc_ind + 10)].hex;
+		if (ft_is_reg(r1, r1, r1))
+		{
+			cursor->reg[r1] = get_dir(e, cursor, 2, 4) &
+				get_dir(e, cursor, 6, 4);
+			cursor->carry = 1;
+		}
+			cursor->index += 11;
+	}
+	else if (ZMASK(acb) == DIR)
+	{
+		r1 = e->a[MODA(opc_ind + 8)].hex;
+		if (ft_is_reg(r1, r1, r1))
+		{
+			cursor->reg[r1] = get_dir(e, cursor, 2, 4) &
+				get_ind(e, cursor, 6);
+			cursor->carry = 1;
+		}
+			cursor->index += 9;
+	}
+	else if (ZMASK(acb) == IIR)
+	{
+		r1 = e->a[MODA(opc_ind + 6)].hex;
+		if (ft_is_reg(r1, r1, r1))
+		{
+			cursor->reg[r1] = get_ind(e, cursor, 2) &
+				get_ind(e, cursor, 4);
+			cursor->carry = 1;
+		}
+			cursor->index += 7;
+	}
+	else if (ZMASK(acb) == IDR)
+	{
+		r1 = e->a[MODA(opc_ind + 8)].hex;
+		if (ft_is_reg(r1, r1, r1))
+		{
+			cursor->reg[r1] = get_ind(e, cursor, 2) &
+				get_dir(e, cursor, 4, 4);
+			cursor->carry = 1;
+		}
+			cursor->index += 9;
 	}
 }
