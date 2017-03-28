484e23c40aefb1aae604a4c585d65535c7653cf0
HAZIZA <mhaziza@student.42.fr>
2017-03-28 19:56:19 +0200

    zjump debug

--- VM/funcs/ft_zjmp.c
+++ VM/funcs/ft_zjmp.c
@@ -6,7 +6,7 @@
 /*   By: nsabbah <nsabbah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/20 17:14:24 by nsabbah           #+#    #+#             */
-/*   Updated: 2017/03/28 19:49:39 by mhaziza          ###   ########.fr       */
+/*   Updated: 2017/03/28 19:56:00 by mhaziza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ void	ft_zjmp(t_env *e, t_cursor *cursor)
 	{
 		index_extra = get_dir(e, cursor, 1, 2);
 		index_extra = MODX(index_extra);
-		if(index_extra == 0)
-			index_extra++;
 		ft_update_cursor(e, cursor, index_extra);
 	}
 	else
