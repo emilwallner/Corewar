45c753e8adcfa840f2ed1aeeea7d208325631a0c
Timothy LENGLINE <tlenglin@e1r3p5.42.fr>
2017-03-29 09:26:38 +0200

    add the mod in the aff function and increase the jump in update_cursor of the function

--- VM/funcs/ft_aff.c
+++ VM/funcs/ft_aff.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ldi.c                                           :+:      :+:    :+:   */
+/*   ft_aff.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nsabbah <nsabbah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/20 17:14:24 by nsabbah           #+#    #+#             */
-/*   Updated: 2017/03/24 17:25:40 by nsabbah          ###   ########.fr       */
+/*   Updated: 2017/03/29 09:24:16 by tlenglin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ void	ft_aff(t_env *e, t_cursor *cursor)
 {
 	char	index_extra;
 
-	index_extra = e->a[cursor->index + 1].hex;
+	index_extra = e->a[MODA(cursor->index + 1)].hex;
 	index_extra = index_extra % 256;
 	if(e->bonus)
 		printw("%c", index_extra);
 	else
 		ft_printf("%c", index_extra);
-	ft_update_cursor(e, cursor, 2);
+	ft_update_cursor(e, cursor, 3);
 }
