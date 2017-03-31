ecf46e52eacdb4dd1be5365e217e1062246e256c
Natansab <natansab@gmail.com>
2017-03-31 11:23:10 +0200

    normed + Wall

--- VM/srcs/ft_check_args.c
+++ VM/srcs/ft_check_args.c
@@ -6,7 +6,7 @@
 /*   By: nsabbah <nsabbah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/19 18:26:25 by nsabbah           #+#    #+#             */
-/*   Updated: 2017/03/30 20:06:13 by mhaziza          ###   ########.fr       */
+/*   Updated: 2017/03/31 11:21:40 by nsabbah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,50 +23,51 @@
 ** data that we get
 */
 
-int	ft_check_args(t_env e, t_cursor cursor)
+int	ft_check_args_2(t_env e, t_cursor cursor, int nb_of_args)
 {
-	int		nb_of_args;
 	int		i;
-	char	opcode;
+	int		opcode;
 	char	acb;
 
-	i = -1;
-	nb_of_args = 0;
-	opcode = e.a[cursor.index].hex;
+	opcode = ZMASK(e.a[cursor.index].hex);
 	acb = e.a[cursor.index + 1].hex;
-// printf("for opcode is %i and acb is %#x\n", opcode, acb);
-
-	if (e.op_tab[opcode].acb == 0)
-		return (1);
-	if (acb & 0x3)
-		return (0);
-		// printf("ERROR\n");
-	while (++i < 3)
-		if (acb >> (6 - i * 2) & 0x3)
-			nb_of_args++;
-	if (nb_of_args != e.op_tab[opcode].nb_params)
-		return (0);
-	// 	printf("ERROR NB OF ARGS");
-	// printf("nb_of_args is %i\n", nb_of_args);
 	i = 0;
 	while (i < nb_of_args)
 	{
 		if ((acb >> (6 - i * 2) & 0x3) == REG_CODE &&
 				(e.op_tab[opcode].params_type[i] & T_REG) == T_REG)
-				;
-			// printf("REG\n");
+			;
 		else if ((acb >> (6 - i * 2) & 0x3) == DIR_CODE &&
 				(e.op_tab[opcode].params_type[i] & T_DIR) == T_DIR)
-				;
-			// printf("DIR\n");
+			;
 		else if ((acb >> (6 - i * 2) & 0x3) == IND_CODE &&
 				(e.op_tab[opcode].params_type[i] & T_IND) == T_IND)
-				;
-			// printf("IND\n");
+			;
 		else
 			return (0);
-			// printf("ERROR ARG - acb >> (6 - i * 2) is %i\n", acb >> (6 - i * 2));
 		i++;
 	}
 	return (1);
+}
+
+int	ft_check_args(t_env e, t_cursor cursor)
+{
+	int		nb_of_args;
+	int		i;
+	int		opcode;
+	char	acb;
+
+	i = -1;
+	opcode = ZMASK(e.a[cursor.index].hex);
+	acb = e.a[cursor.index + 1].hex;
+	if (e.op_tab[opcode].acb == 0)
+		return (1);
+	if (acb & 0x3)
+		return (0);
+	while (++i < 3)
+		if (acb >> (6 - i * 2) & 0x3)
+			nb_of_args++;
+	if (nb_of_args != e.op_tab[opcode].nb_params)
+		return (0);
+	return (ft_check_args_2(e, cursor, nb_of_args));
 }
